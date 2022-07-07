98dfab5e2a812054efdff3c7f07e30d3723b37ae
Kninja99 <kcdamschen@yahoo.com>
2022-07-07 15:07:15 -0700

    updates from local

--- CSS_Sorter.py
+++ CSS_Sorter.py
@@ -1,41 +1,54 @@
 import collections
-css_file = open("test.css", 'r')
-#content = css_file.read()
+import os
+from os.path import exists
 
-lines = css_file.readlines()
-# a alphabetical dictonary 
-organizer = {}
-bracket_counter = 0
-# css element split up line byt line 
-css_ele = []
-for line in lines:
-    curr_line = line.replace(' ', '')
-    if curr_line != '' and curr_line.split() :
-        # adds line to the current element
-        css_ele.append(line)
-        curr_line = curr_line.replace('\n', '')
-        # check to see if its at the end of a element 
-        if curr_line[len(curr_line) - 1] == '{':
-            bracket_counter += 1
-        elif curr_line[0] == '}':
-            bracket_counter -= 1
-        if curr_line[0] == '}' and bracket_counter <= 0:
-            start = css_ele[0]
-            # replaceing selectors
-            start = start.replace('.', '')
-            start = start.replace('#', '')
-            # copying the array to it points to a different memory address for all elements
-            entry = css_ele.copy()
-            # check if the element needs to create a new key
-            if start[0] in organizer:
-                organizer[start[0]].append(entry)
-            else:
-                organizer[start[0]] = [entry]
-            # clearing current css element for new element
-            css_ele.clear()
+def sortCss(css_path):
+    css_file = open(css_path, 'r')
+    #content = css_file.read()
 
-ordered_organizer = collections.OrderedDict(sorted(organizer.items()))
+    lines = css_file.readlines()
+    css_file.close()
+    # a alphabetical dictonary 
+    organizer = {}
+    bracket_counter = 0
+    # css element split up line byt line 
+    css_ele = []
+    for line in lines:
+        curr_line = line.replace(' ', '')
+        if curr_line != '' and curr_line.split() :
+            # adds line to the current element
+            css_ele.append(line)
+            curr_line = curr_line.replace('\n', '')
+            # check to see if its at the end of a element 
+            if curr_line[len(curr_line) - 1] == '{':
+                bracket_counter += 1
+            elif curr_line[0] == '}':
+                bracket_counter -= 1
+                css_ele[len(css_ele) - 1] = css_ele[len(css_ele) - 1] + '\n'
+            if curr_line[0] == '}' and bracket_counter <= 0:
+                start = css_ele[0]
+                # replaceing selectors
+                start = start.replace('.', '')
+                start = start.replace('#', '')
+                # copying the array to it points to a different memory address for all elements
+                entry = css_ele.copy()
+                # check if the element needs to create a new key
+                if start[0] in organizer:
+                    organizer[start[0]].append(entry)
+                else:
+                    organizer[start[0]] = [entry]
+                # clearing current css element for new element
+                css_ele.clear()
 
-for key in ordered_organizer:
-    print(ordered_organizer[key])
-css_file.close()
+    ordered_organizer = collections.OrderedDict(sorted(organizer.items()))
+    pwd = os.getcwd()
+    # removes old sorted css
+    if exists(f'{pwd}/sorted.css'):
+        os.remove(f'{pwd}/sorted.css')
+    css_sorted = open('sorted.css', 'w')
+    for key in ordered_organizer:
+        #grabs each array of elements that start with this key
+        for arr in ordered_organizer[key]:
+            # writes each element
+            for element in arr:
+                css_sorted.write(element)
